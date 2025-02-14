828c2d76368e308f088ff6cb08e17f47c45829bf
Rayane <108083645+The-soulless12@users.noreply.github.com>
2025-02-14 08:11:42 +0100

    Create detection.py

---
+++ detection.py
@@ -0,0 +1,16 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# Charger le modèle entraîné
+model = YOLO("runs/train/exp/weights/best.pt")  # Vérifie le chemin exact du modèle sauvegardé
+
+# Charger une image de rayonnage
+image_path = "Photos rayonnages/image_test.jpg"
+image = cv2.imread(image_path)
+
+# Effectuer la détection
+results = model(image_path)
+
+# Afficher les résultats
+for result in results:
+    result.show()  # Affichage interactif
